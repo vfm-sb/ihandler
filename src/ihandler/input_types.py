4c0a07646689f79fede6425be81368103248742c
VFM | SB <vfm_sb@proton.me>
2024-02-29 17:12:56 +0200

    String Inputs >> Included Functional assert_input() & UnrestrictedStringInput
    > Applicable pyvutils
    
    modified:   .gitignore
    modified:   pyproject.toml
    modified:   requirements.txt
    modified:   src/ihandler/input_types.py
    modified:   src/ihandler/string_inputs.py

--- src/ihandler/input_types.py
+++ src/ihandler/input_types.py
@@ -1,11 +1,12 @@
 from ihandler.string_inputs import (
-    StrictStringInput,
+    StrictStringInput, UnrestrictedStringInput,
     LowercaseStringInput, UppercaseStringInput
 )
 
 
 INPUT_TYPES = {
     "strict-string": StrictStringInput,
+    "unrestricted-string": UnrestrictedStringInput,
     "lower-string": LowercaseStringInput,
     "upper-string": UppercaseStringInput,
 }
--- src/ihandler/string_inputs.py
+++ src/ihandler/string_inputs.py
@@ -1,11 +1,18 @@
 
+# Local Modules
+import pyvutils
 from ihandler.base_input import BaseInput
 
 
 class StrictStringInput(BaseInput):
     def input_processor(self) -> str:
         user_input = super().input_processor()
+        pyvutils.assert_input(user_input)
         return user_input.strip()
+
+
+class UnrestrictedStringInput(BaseInput):
+    pass
 
 
 class LowercaseStringInput(StrictStringInput):
